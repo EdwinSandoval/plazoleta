6415ff5ccd2b8670fac1545bd81b8ecffec46b6a
EdwinSandoval <edwin.sandovalv@pragma.com.co>
2023-07-25 17:01:05 -0500

    Implementacion de CI con githubAction

--- src/main/java/com/example/serviceplazoleta/application/dto/request/RestauranteRequestDto.java
+++ src/main/java/com/example/serviceplazoleta/application/dto/request/RestauranteRequestDto.java
@@ -9,23 +9,23 @@ import javax.validation.constraints.Pattern;
 @Getter
 @Setter
 public class RestauranteRequestDto {
-    @NotBlank(message = "El campo nombre es obligatorio")
+    //@NotBlank(message = "El campo nombre es obligatorio")
     private String nombre;
 
-    @NotBlank(message = "El campo direccion es obligatorio")
+   // @NotBlank(message = "El campo direccion es obligatorio")
     private String direccion;
 
-    @NotBlank(message = "El campo telefono es obligatorio")
-    @Pattern(regexp = "^[0-9]*$", message = "El telefono debe ser numerico")
+   // @NotBlank(message = "El campo telefono es obligatorio")
+   // @Pattern(regexp = "^[0-9]*$", message = "El telefono debe ser numerico")
     private String telefono;
 
-    @NotBlank(message = "El campo urlLogo es obligatorio")
+   // @NotBlank(message = "El campo urlLogo es obligatorio")
     private String urlLogo;
 
-    @NotBlank(message = "El campo nit es obligatorio")
-    @Pattern(regexp = "^[0-9]*$", message = "El documento de identidad debe ser numerico")
+  //  @NotBlank(message = "El campo nit es obligatorio")
+  //  @Pattern(regexp = "^[0-9]*$", message = "El documento de identidad debe ser numerico")
     private String nit;
 
-    @NotBlank(message = "El campo idPropietario es obligatorio")
+  //  @NotBlank(message = "El campo idPropietario es obligatorio")
     private Long idPropietario;
 }
